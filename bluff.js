5b0ee50494fb6ee51309d21a9112e5a5ecd7fd21
NenaMic <57640428+NenaMic@users.noreply.github.com>
2022-11-20 22:46:22 +0100

    Connected to local db
    Used local db to insert new user

--- bluff.js
+++ bluff.js
@@ -1,56 +1,80 @@
 "use strict";
-const express = require('express');
+const express = require("express");
+const mysql = require("@mysql/xdevapi");
 const app = express();
+
 const PORT = 30000;
-// const mysql = require('@mysql/xdevapi');
 
-// const config = {
-//     host: 'it113752@users.iee.ihu.gr',
-//     port: 33060,
-//     user: 'root',
-//     password: 'Pmic93nena!',
-//     schema: 'bluff',
-//     socket: '/home/student/it/2011/it113752/mysql/run/mysql.sock'
-// }
+const config = {
+    user: "root",
+    password: "Pmic93nena!",
+    schema: "bluff",
+};
 
-// mysql.getSession(config).then((session) => {
-//     console.log(session.close);
-// }, (error) => {
-//     console.log(error)
-// });
-app.get('/rules', (_req, res) => {
-    res.send({
-        rules: 'Καλως ηρθατε στην Μπλοφα. Οι κανονες ειναι: ' +
-            '1. Το παιχνιδι παιζεται με 3 παικτες. ' +
-            '2. Χρησιμοποιειται μονο μια τραπουλα σε καθε παιχνιδι. ' +
-            '3. Σε καθε γυρο ο παικτης πρεπει να ανακοινωσει ποια χαρτια ' +
-            'θελει να πεταξει, ποσα χαρτια + ποιο ειδος χαρτιου, π.χ. 3 βαλεδες. ' +
-            'Αυτα τα χαρτια δεν ειναι απαραιτητο να ταιριαζουν με τα χαρτια που οντως ' +
-            'θα ριξει.'
+shapes.forEach(shape => {
+    symbols.forEach(symbol => {
+        console.log(`INSERT INTO bluff.card (symbol_id, shape_id) VALUES ((SELECT id FROM bluff.card_symbol WHERE name = "${symbol}"), (SELECT id FROM bluff.card_shape WHERE name = "${shape}"));`);
     })
-});
+})
 
-app.get('/login', (req, res) => {
-    const name = req.query.name;
+const handleError = (res, error, endpoint) => {
+    console.error(error);
+    res.send({ error, endpoint });
+}
 
-    if (!name) {
-        res.send({ error: "A player name needs to be provided", endpoint: "GET /login?name=" });
-        return;
+mysql.getSession(config).then(
+    (session) => {
+        app.get("/rules", (_req, res) => {
+            res.send({
+                rules:
+                    "Καλως ηρθατε στην Μπλοφα. Οι κανονες ειναι: " +
+                    "1. Το παιχνιδι παιζεται με 3 παικτες. " +
+                    "2. Χρησιμοποιειται μονο μια τραπουλα σε καθε παιχνιδι. " +
+                    "3. Σε καθε γυρο ο παικτης πρεπει να ανακοινωσει ποια χαρτια " +
+                    "θελει να πεταξει, ποσα χαρτια + ποιο ειδος χαρτιου, π.χ. 3 βαλεδες. " +
+                    "Αυτα τα χαρτια δεν ειναι απαραιτητο να ταιριαζουν με τα χαρτια που οντως " +
+                    "θα ριξει.",
+            });
+        });
+
+        app.get("/login", (req, res) => {
+            const name = req.query.name;
+
+            if (!name) {
+                handleError(res, error, "GET /login?name=");
+                return;
+            }
+
+            // insert new user to db
+            session.sql(`INSERT INTO user (name) VALUES ("${name}");`)
+                .execute()
+                .then((result) => {
+                    const userId = result.getAutoIncrementValue();
+
+                    // check which games await for users (not full)
+                    session.sql('SELECT game_id, COUNT(*) as count from game_user_sequence GROUP BY game_id HAVING count < 3;')
+                        .execute()
+                        .then((result) => {
+                            const availableGameIds = result.fetchAll();
+
+                            res.send({
+                                message:
+                                    `You logged in successfully. Your unique userId is ${userId}. ` +
+                                    "You need to remember it for the duration of your game. " + `${availableGameIds.length ? `There exist the following available games: ${JSON.stringify(availableGameIds)}. You can either connect to one of them, or create a new game` : 'There are no available games at the moment. Please create a new game!'}`,
+                                userId,
+                                name,
+                                availableGameIds,
+                            });
+                        });
+                }, error => {
+                    handleError(res, error, "GET /login?name=");
+                });
+        });
+    },
+    (error) => {
+        console.error(error);
     }
-
-    // create user in user table and return unique ide
-    // INSERT INTO user (name) VALUES (${name});
-    // SELECT LAST_INSERT_ID();
-
-    // SELECT game_id, COUNT(*) as count from game_user_sequence GROUP BY game_id HAVING count < 3;
-
-    const availableGameIds = [1, 2, 3];
-    const userId = 1234;
-    res.send({
-        message: `You logged in successfully. Your unique userId is ${userId}. ` +
-            'You need to remember it for the duration of your game', userId, name, availableGameIds
-    })
-});
+);
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}`);
